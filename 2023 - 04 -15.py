7c7a30d81268312873975318b381ad74fed1b425
kangsanghyeo <120238777+kangsanghyeo@users.noreply.github.com>
2023-04-15 17:52:01 +0900

    Add files via upload

---
+++ 2023 - 04 -15.py
@@ -0,0 +1,30 @@
+import requests
+
+def remove_first_word(src, word):
+      pos = src.find(word) + len(word)
+      src = src[pos:]
+      return src
+
+def change_number( url):
+      headers = {'User-Agent':'Mozilla/5.0 (windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'}
+      site = requests.get(url, headers=headers)
+      source_data = site.text
+      return source_data
+
+for q in range(1, 7, 1):
+      source_data = change_number('https://comic.naver,com/webtoon/detail?titleId=807314&no={0}'.format(q))
+      #url = 'https://comic.naver.com/webtoon/detail?titleId=807314&no={0}'.format(q)
+      site = requests.get(url, headers=headers)
+      source_data = site.text
+
+      source_data = remove_first_word(source_data, 'title" content="')
+      #pos1 = source_data.find('title" content="')+ len('title" content="')
+      #source_data = source_data[pos1:]
+
+      pos2 = source_data.find('">')
+      a_data = source_data[: pos2].strip()
+
+      print(a_data)
+            
+      
+    
